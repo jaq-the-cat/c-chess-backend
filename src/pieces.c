415fa84123542ed40b6dd556b881187842727f88
jaq-cat <jaq.cat@protonmail.ch>
2020-12-05 17:32:35 -0300

    bishopy boi

--- src/pieces.c
+++ src/pieces.c
@@ -8,14 +8,15 @@ Piece make_piece(PieceTypes type) {
     switch(type) {
         case Pawn: {
             const int MOVES = 3;
-            Point m[MOVES] = MOVES(POINT(1, 0), POINT(-1, 0), POINT(0, 1));
+            Point m[MOVES] = MOVES(POINT(MOVE_1, 0), POINT(-MOVE_1, 0), POINT(0, MOVE_1));
             for (int i=0; i<3; i++)
                 p.moves[i] = m[i];
             break;
         }
         case Bishop: {
-            const int MOVES = 3;
-            Point m[MOVES] = MOVES(POINT(1, 0), POINT(-1, 0), POINT(0, 1));
+            const int MOVES = 4;
+            Point m[MOVES] = MOVES(POINT(MOVE_INF, MOVE_INF), POINT(-MOVE_INF, -MOVE_INF), POINT(MOVE_INF, -MOVE_INF),
+                    POINT(-MOVE_INF, MOVE_INF));
             for (int i=0; i<MOVES; i++)
                 p.moves[i] = m[i];
             break;
