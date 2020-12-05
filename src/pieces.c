75ccb595f4bdcbd163ec81a18902e78a26c4419f
jaq-cat <jaq.cat@protonmail.ch>
2020-12-05 17:28:25 -0300

    ay

--- src/pieces.c
+++ src/pieces.c
@@ -7,23 +7,45 @@ Piece make_piece(PieceTypes type) {
     Piece p = PIECE_T(type);
     switch(type) {
         case Pawn: {
-            Point m[MOVE_COUNT] = MOVES(POINT(1, 0), POINT(-1, 0), POINT(0, 1));
-            for (int i=0; i<MOVE_COUNT; i++);
+            const int MOVES = 3;
+            Point m[MOVES] = MOVES(POINT(1, 0), POINT(-1, 0), POINT(0, 1));
+            for (int i=0; i<3; i++)
+                p.moves[i] = m[i];
             break;
         }
         case Bishop: {
+            const int MOVES = 3;
+            Point m[MOVES] = MOVES(POINT(1, 0), POINT(-1, 0), POINT(0, 1));
+            for (int i=0; i<MOVES; i++)
+                p.moves[i] = m[i];
             break;
         }
         case Knight: {
+            const int MOVES = 3;
+            Point m[MOVES] = MOVES(POINT(1, 0), POINT(-1, 0), POINT(0, 1));
+            for (int i=0; i<MOVES; i++)
+                p.moves[i] = m[i];
             break;
         }
         case Rook: {
+            const int MOVES = 3;
+            Point m[MOVES] = MOVES(POINT(1, 0), POINT(-1, 0), POINT(0, 1));
+            for (int i=0; i<MOVES; i++)
+                p.moves[i] = m[i];
             break;
         }
         case Queen: {
+            const int MOVES = 3;
+            Point m[MOVES] = MOVES(POINT(1, 0), POINT(-1, 0), POINT(0, 1));
+            for (int i=0; i<MOVES; i++)
+                p.moves[i] = m[i];
             break;
         }
         case King: {
+            const int MOVES = 3;
+            Point m[MOVES] = MOVES(POINT(1, 0), POINT(-1, 0), POINT(0, 1));
+            for (int i=0; i<MOVES; i++)
+                p.moves[i] = m[i];
             break;
         }
     }
