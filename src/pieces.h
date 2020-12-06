2f0511d846ed0bb469c6e80702dd570b66ccc814
jaq-cat <jaq.cat@protonmail.ch>
2020-12-06 10:14:51 -0300

    knight

--- src/pieces.h
+++ src/pieces.h
@@ -1,8 +1,7 @@
 #ifndef PIECES_H
 #define PIECES_H
 
-#define M1 1
-#define MINF 2
+#define MINF -1
 
 #define POINT(X, Y) (Point) { X, Y }
 #define MOVES(...) { __VA_ARGS__ }
@@ -12,7 +11,7 @@
 
 #define PAWN_L 3
 #define BISHOP_L 4
-#define KNIGHT_L 3
+#define KNIGHT_L 8
 #define ROOK_L 4
 #define QUEEN_L 8
 #define KING_L 8
@@ -50,14 +49,22 @@ typedef const Point Moves[MOVE_COUNT];
 Piece make_piece(PieceTypes, PieceTeam, int);
 
 // Moves
-static Moves PAWN_M = MOVES(POINT(M1, 0), POINT(-M1, 0), POINT(0, M1));
+static Moves PAWN_M = MOVES(POINT(1, 0), POINT(-1, 0), POINT(0, 1));
+
+static Moves ROOK_M = MOVES(POINT(MINF, 0), POINT(-MINF, 0), POINT(0, MINF), POINT(0, -MINF));
+
+static Moves KNIGHT_M = MOVES(
+        // left
+        POINT( 2,  1), POINT( 2, -1), 
+        // right
+        POINT(-2,  1), POINT(-2, -1), 
+        // up
+        POINT(-1,  2), POINT( 1,  2), 
+        // down
+        POINT(-1, -2), POINT( 1, -2));
 
 static Moves BISHOP_M = MOVES(POINT(MINF, MINF), POINT(-MINF, -MINF), POINT(MINF, -MINF),
         POINT(-MINF, MINF));
-
-static Moves KNIGHT_M = MOVES(POINT(1, 0), POINT(-1, 0), POINT(0, 1));
-
-static Moves ROOK_M = MOVES(POINT(MINF, 0), POINT(-MINF, 0), POINT(0, MINF), POINT(0, -MINF));
 
 static Moves QUEEN_M = MOVES(
         POINT(-MINF,  MINF), POINT(0,  MINF), POINT(MINF,  MINF),
@@ -65,8 +72,8 @@ static Moves QUEEN_M = MOVES(
         POINT(-MINF, -MINF), POINT(0, -MINF), POINT(MINF, -MINF));
 
 static Moves KING_M = MOVES(
-        POINT(-M1,  M1), POINT(0,  M1), POINT(M1,  M1),
-        POINT(-M1,  0),                 POINT(M1,  0),
-        POINT(-M1, -M1), POINT(0, -M1), POINT(M1, -M1));
+        POINT(-1,  1), POINT(0,  1), POINT(1,  1),
+        POINT(-1,  0),                 POINT(1,  0),
+        POINT(-1, -1), POINT(0, -1), POINT(1, -1));
 
 #endif
