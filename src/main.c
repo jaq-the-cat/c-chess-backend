2623eda30de81c00fd052b4bb7c402b66be079ef
jaq-cat <jaq.cat@protonmail.ch>
2020-12-05 22:45:27 -0300

    yay

--- src/main.c
+++ src/main.c
@@ -9,10 +9,25 @@ void make_game() {
     for (int x=0; x<BOARD_SIZE; x++) {
         p = make_piece(Pawn, White, x);
         board[x + BOARD_SIZE*p.y] = p;
+
         p = make_piece(Pawn, Black, x);
         board[x + BOARD_SIZE*p.y] = p;
     }
+
     // Rooks
+    // left
+    p = make_piece(Rook, White, 0);
+    board[0 + BOARD_SIZE*p.y] = p;
+
+    p = make_piece(Rook, Black, BOARD_SIZE-1);
+    board[BOARD_SIZE-1 + BOARD_SIZE*p.y] = p;
+
+    // right
+    p = make_piece(Pawn, White, 0);
+    board[0 + BOARD_SIZE*p.y] = p;
+
+    p = make_piece(Pawn, Black, BOARD_SIZE-1);
+    board[BOARD_SIZE-1 + BOARD_SIZE*p.y] = p;
 
     for (int y=0; y<BOARD_SIZE; y++) {
         for (int x=0; x<BOARD_SIZE; x++)
