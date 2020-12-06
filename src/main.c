5f5449c5ea680b07cba5b5f9f8b149799cc4543d
jaq-cat <jaq.cat@protonmail.ch>
2020-12-06 09:47:57 -0300

    game.c/h

--- src/main.c
+++ src/main.c
@@ -1,41 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "pieces.h"
-#include "board.h"
-
-void make_game() {
-    Board board = {0};
-    Piece p;
-    // Pawns
-    for (int x=0; x<BOARD_SIZE; x++) {
-        p = make_piece(Pawn, White, x);
-        board[x + BOARD_SIZE*p.y] = p;
-
-        p = make_piece(Pawn, Black, x);
-        board[x + BOARD_SIZE*p.y] = p;
-    }
-
-    // Rooks
-    // left
-    p = make_piece(Rook, White, 0);
-    board[0 + BOARD_SIZE*p.y] = p;
-
-    p = make_piece(Rook, Black, BOARD_SIZE-1);
-    board[0 + BOARD_SIZE*p.y] = p;
-
-    // right
-    p = make_piece(Rook, White, 0);
-    board[BOARD_SIZE-1 + BOARD_SIZE*p.y] = p;
-
-    p = make_piece(Rook, Black, BOARD_SIZE-1);
-    board[BOARD_SIZE-1 + BOARD_SIZE*p.y] = p;
-
-    for (int y=0; y<BOARD_SIZE; y++) {
-        for (int x=0; x<BOARD_SIZE; x++)
-            printf("%d ", board[x + BOARD_SIZE*y].type);
-        printf("\n");
-    }
-}
+#include "game.h"
 
 int main() {
     make_game();
